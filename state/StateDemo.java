7507c9f650f8728c3b23a069a59633ee04d987e2
raksit <rak-sit@hotmail.com>
2017-01-25 18:02:39 +0700

    State Pattern

---
+++ state/StateDemo.java
@@ -0,0 +1,21 @@
+package state;
+
+public class StateDemo {
+
+	public static void main(String[] args) {
+		
+		Context context = new Context();
+		
+		State startState = new StartState();
+		
+		startState.doAction(context);
+		
+		System.out.println(context.getState().toString());
+		
+		State stopState = new StopState();
+		
+		stopState.doAction(context);
+		
+		System.out.println(context.getState().toString());
+	}
+}
